21eb5e416ebd8fbe4affb36416c230c9d88b6c8d
DESKTOP-3GUQ93R\GA2A <h1202415024@hiratagakuen.onmicrosoft.com>
2025-06-26 17:32:41 +0900

    aa

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+// 力士を動かすためのスクリプト
+public class Controller : MonoBehaviour
+{
+    // 力士の移動する速さ
+    public float moveSpeed = 5f;
+
+    // コンポーネントを入れる場所
+    Rigidbody2D rb;
+
+    // 変数
+    Vector2 movement;
+
+    void Start()
+    {
+        // ゲームが始まったらRigidbody2Dに入れる
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // キーボードの左右をmovement.xに入れる
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    void FixedUpdate()
+    {
+        // Rigidbody2Dを使って力士を動かす
+        //動く方向と速さをかけた分だけ移動させる
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Script/Dohyou.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+
+public class Dohyou : MonoBehaviour
+{
+    // —h‚ê‚é‘å‚«‚³
+    public float shakeAmount = 0.1f;
+
+    // —h‚ê‚é‘¬‚³
+    public float shakeSpeed = 5f;
+
+    // ‰ŠúˆÊ’u‚ğŠo‚¦‚Æ‚­•Ï”
+    Vector3 startPos;
+
+    // —h‚ê‚Ä‚é‚©‚Ç‚¤‚©‚Ì”»’è
+    bool shaking = false;
+
+    void Start()
+    {
+        
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // ƒXƒy[ƒXƒL[‚ğP‚½‚Æ‚«—h‚ê‚éor~‚Ü‚é
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shaking = !shaking;  // true—h‚ê‚éAfalse‚È‚ç~‚Ü‚é‚æ‚¤‚É‚·‚é
+        }
+
+        if (shaking)
+        {
+            // X‚ÆY•ûŒü‚É—h‚ç‚·ŒvZ
+            float offsetX = Mathf.Sin(Time.time * shakeSpeed) * shakeAmount;
+            float offsetY = Mathf.Cos(Time.time * shakeSpeed * 0.7f) * shakeAmount;
+
+            // ŒvZ‚µ‚½—h‚ê‚ğ“y•U‚ÌˆÊ’u‚É‘«‚µ‚Ä“®‚©‚·
+            transform.position = startPos + new Vector3(offsetX, offsetY, 0);
+        }
+        else
+        {
+            // —h‚ê‚Ä‚È‚¢‚Æ‚«‚ÍŒ³‚ÌêŠ‚É–ß‚·
+            transform.position = startPos;
+        }
+    }
+}
